6cc9f0f749c5c9392245d2a7a8cb134c0101656b
FlyingPig278 <flyingpig278@gmail.com>
2026-04-29 23:11:51 +0800

    Initial Commit

---
+++ src/main/java/cn/xducraft/hide_and_seek/Hide_and_seek.java
@@ -0,0 +1,16 @@
+package cn.xducraft.hide_and_seek;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Hide_and_seek extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
